0a8447833a0a1165270a79893dd165c079b27090
Ivan Gelo <ivangelo@Ivans-MacBook-Pro.local>
2023-11-26 17:53:18 +0100

    mult + fill

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,54 @@
 // 'white' in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-//// Replace this comment with your code.
+(LOOP)
+@SCREEN
+D=A
+@addr
+M=D
+
+(CONDITION)
+@KBD
+D=M
+@BLACK
+D;JGT	// if
+@WHITE
+D;JEQ	//ELSE JUMP TO WHITEN
+
+@CONDITION
+0;JMP
+
+(BLACK)
+@fill
+M=-1	// black fill
+@CHANGE
+0;JMP
+
+(WHITE)
+@fill
+M=0	// white fill
+@CHANGE
+0;JMP
+
+(CHANGE)
+@fill
+D=M
+
+@addr
+A=M
+M=D
+
+@addr
+D=M+1	// inc to next pixel
+@KBD
+D=A-D
+
+@addr
+M=M+1	// inc to next pixel
+A=M
+
+@CHANGE
+D;JGT	// while is greater than 0, screen is not painted
+
+@LOOP
+0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,4 +7,61 @@
 // Assumes that R0 >= 0, R1 >= 0, and R0 * R1 < 32768.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-//// Replace this comment with your code.
+    // initalize i to R0
+    @R0
+    D=M
+    @i
+    M=D
+
+    // initialize R2 to 0
+    @R2
+    M=0
+
+    // if R0 == 0; jump to cleanup
+    @R0
+    D=M
+    @CLEANUP
+    D;JEQ
+
+    // if R1 == 0; jump to cleanup
+    @R1
+    D=M
+    @CLEANUP
+    D;JEQ
+
+
+(LOOP)
+    // R2 = R2 + R1
+    @R1
+    D=M
+    @R2
+    M=M + D
+    // decrement i
+    @i
+    M=M - 1
+    D=M
+    // if i > 0; loop
+    @LOOP
+    D;JGT
+    // if i == 0; terminate program
+    @CLEANUP
+    D;JLE
+
+(CLEANUP)
+    // set R0 to 0
+    @R0
+    M=0
+    // set R1 to 0
+    @R1
+    M=0
+    // set i to 0
+    @i
+    M=0
+    // jump to end
+    @END
+    0;JMP
+
+    // infinite loop
+(END)
+    @END
+    0;JMP
